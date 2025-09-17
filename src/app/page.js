163c7acf7fe88754becac8492a3771033000933d
Pili <pilinogo@yahoo.com>
2025-09-18 01:10:02 +0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,21 @@
 export default function Home() {
   return (
-  <div>
-    <h1>Hi!</h1>
-  </div>
+    <div style={{ padding: '2rem', fontFamily: 'Arial, sans-serif', lineHeight: '2' }}>
+      <p>
+        <a href="https://excuse-gen-api.vercel.app/api/excuse" target="_blank" rel="noopener noreferrer">Excuse API</a>
+      </p>
+      <p>
+        <a href="https://excuse-gen-api.vercel.com/excuse.exe" target="_blank" rel="noopener noreferrer">Excuse EXE</a>
+      </p>
+      <p>
+        <a href="https://excuse-gen-api.vercel.com/excuse.apk" target="_blank" rel="noopener noreferrer">Excuse APK</a>
+      </p>
+      <p>
+        <a href="https://ayopili.com/terminal" target="_blank" rel="noopener noreferrer">Excuse CLI*</a>
+      </p>
+      <p style={{ fontStyle: 'italic', fontSize: '0.9rem' }}>
+        *Type &quot;pili excuse&quot; within the terminal to fetch an excuse.
+      </p>
+    </div>
   );
 }
