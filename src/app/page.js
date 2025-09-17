1d3870d031bf576e554e7007fb2d5ab8bd34b70b
Pili <85771394+Piliii@users.noreply.github.com>
2025-09-18 00:49:40 +0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,21 @@
 export default function Home() {
   return (
-  <div>
-    <h1>Hi!</h1>
-  </div>
+    <div style={{ padding: '2rem', fontFamily: 'Arial, sans-serif', lineHeight: '2' }}>
+      <p>
+        (Excuse API)[<a href="https://excuse-gen-api.vercel.app/api/excuse" target="_blank" rel="noopener noreferrer">Link</a>]
+      </p>
+      <p>
+        (Excuse EXE)[<a href="https://excuse-gen-api.vercel.com/excuse.exe" target="_blank" rel="noopener noreferrer">Link</a>]
+      </p>
+      <p>
+        (Excuse APK)[<a href="https://excuse-gen-api.vercel.com/excuse.apk" target="_blank" rel="noopener noreferrer">Link</a>]
+      </p>
+      <p>
+        (Excuse CLI*)[<a href="https://ayopili.com/terminal" target="_blank" rel="noopener noreferrer">Link</a>]
+      </p>
+      <p style={{ fontStyle: 'italic', fontSize: '0.9rem' }}>
+        *Type "pili excuse" to fetch an excuse.
+      </p>
+    </div>
   );
 }
